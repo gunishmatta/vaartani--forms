7bacd3cf8978b0137a350c309fb4906eebbede35
gunishmatta <gunishmatta@gmail.com>
2020-09-09 15:21:12 +0530

    Signature pad with button changes

--- src/components/PolicyComponent.js
+++ src/components/PolicyComponent.js
@@ -1,21 +1,26 @@
-import React from 'react';
+import React,{useState} from 'react';
 import { makeStyles } from "@material-ui/core/styles";
 import Paper from "@material-ui/core/Paper";
 import Container from "@material-ui/core/Container";
-import { Header } from "semantic-ui-react";
-
+import { Header,Button } from "semantic-ui-react";
+import SemanticDatepicker from 'react-semantic-ui-datepickers';
+import 'react-semantic-ui-datepickers/dist/react-semantic-ui-datepickers.css';
+ 
 import Typography from '@material-ui/core/Typography';
 import { Slider } from 'antd';
 
-function onChange(value) {
-  console.log('onChange: ', value);
-}
 
-function onAfterChange(value) {
-  console.log('onAfterChange: ', value);
-}
 export default function PolicyComponent() {
 
+  function onChange(value) {
+    setSliderValue(value);
+    console.log('onChange: ', value);
+
+  }
+  
+  function onAfterChange(value) {
+    console.log('onAfterChange: ', value);
+  }
   const useStyles = makeStyles((theme) => ({
     root: {
       display: "flex",
@@ -29,7 +34,9 @@ export default function PolicyComponent() {
     }
   }));
   const classes = useStyles();
-
+const [slidervalue,setSliderValue] = useState(3);
+const [currentDate, setNewDate] = useState(null);
+  const onChangeDate = (event, data) => setNewDate(data.value);
   const marks = {
     1: '1m',
     2: '2m',
@@ -49,15 +56,36 @@ export default function PolicyComponent() {
       <Paper elevation={3}>
 
         <Container className="businesspageheader">
-          <Header textAlign="center" as="h2">
-            Policy Coverage
-          </Header>
+        <Header textAlign="center" as="h2">
+        Policy Coverage
+        </Header> 
+     
+        <Header textAlign="center" as="h1">
+          ${slidervalue}M
+            </Header>
 
         </Container>
-        <div>
+        <div className="slidercontainer">
 
-        <Slider min={1} max={10} marks={marks} step ={1} defaultValue={3} onChange={onChange} onAfterChange={onAfterChange} />
+        <Slider style={{margin : "50px"}} min={1} max={10} marks={marks} step ={1} defaultValue={3} onChange={onChange} onAfterChange={onAfterChange}  />
     
+        </div>
+        <div className="dateheadingcontainer">
+        <h4>Start Date</h4>
+        </div>
+        <div className="datepickercontainer">
+        <div className="datepick">
+        <SemanticDatepicker className="date-picker-class" onChange={onChangeDate} />
+        </div>
+        <Container className="addressformbuttons">
+        <Button  floated="left" style={{margin:"20px"}}>
+          Previous
+   </Button>
+        <Button primary floated="right" style={{margin:"20px"}}>
+          Next
+   </Button>
+      </Container>
+
         </div>
       </Paper>
     </div>
--- src/components/QuoteComponent.js
+++ src/components/QuoteComponent.js
@@ -1,9 +1,27 @@
 import React, { useState, useRef } from "react";
 import Popup from "reactjs-popup";
+import { makeStyles } from "@material-ui/core/styles";
 import SignaturePad from "react-signature-canvas";
+import Paper from "@material-ui/core/Paper";
+import { Button, Form,Header } from "semantic-ui-react";
 import "../sigCanvas.css";
-
+import Container from "@material-ui/core/Container";
+import Logo from "../assets/amazonlogo.png";
 function App() {
+  const useStyles = makeStyles((theme) => ({
+    root: {
+      display: "flex",
+      flexWrap: "wrap",
+      "& > *": {
+        margin: theme.spacing(1),
+        width: theme.spacing(100),
+        height: theme.spacing(100),
+        marginLeft: "17em"
+      }
+    }
+  }));
+  const classes = useStyles();
+
   const [imageURL, setImageURL] = useState(null); // create a state that will contain our image url
 
   const sigCanvas = useRef({});
@@ -19,37 +37,56 @@ function App() {
     setImageURL(sigCanvas.current.getTrimmedCanvas().toDataURL("image/png"));
 
   return (
-    <div className="App">
-      <h1>Payment</h1>
-      <>
+    <div className={classes.root}>
+      <Paper elevation={3}>
+      <div className="logocontainer">
+      <img src={Logo} alt="amazon logo" className="amazon-logo"></img>
+    </div>
+    <Container>
+      <Header textAlign="center" as="h2">
+        Seller's Policy
+      </Header>
+    </Container>
+
+      
+      // <div className="quotepageheading">
+      //     <h1>Payment</h1>
+      //   </div>
+        <>
+     
+      <div className="signaturepadcontainer">
             <SignaturePad
               ref={sigCanvas}
-              canvasProps={{
-                className: "signatureCanvas"
-              }}
+              canvasProps={{ width: 500, height: 200, className: 'sigCanvas' }}
             />
-            {/* Button to trigger save canvas image */}
-            <button onClick={save}>Save</button>
-            <button onClick={clear}>Clear</button>
-          </>
-    
-      <br />
-      <br />
-      {/* if our we have a non-null image url we should 
+          </div>
+          {/* Button to trigger save canvas image */}
+
+          <div className="QuoteButtons">
+            <Button onClick={save}>Save</Button>
+            <Button onClick={clear}>Clear</Button>
+          </div>
+        </>
+
+        <br />
+        <br />
+        {/* if our we have a non-null image url we should 
       show an image and pass our imageURL state to it*/}
-      {imageURL ? (
-        <img
-          src={imageURL}
-          alt="my signature"
-          style={{
-            display: "block",
-            margin: "0 auto",
-            border: "1px solid black",
-            width: "150px"
-          }}
-        />
-      ) : null}
+        {imageURL ? (
+          <img
+            src={imageURL}
+            alt="my signature"
+            style={{
+              display: "block",
+              margin: "0 auto",
+              border: "1px solid black",
+              width: "150px"
+            }}
+          />
+        ) : null}
+      </Paper>
     </div>
+
   );
 }
 

02926a6f81579532c44cd94837861a9b9ef1b2d3
gunishmatta <gunishmatta@gmail.com>
2020-09-13 22:30:01 +0530

    Updates

---
+++ precache-manifest.a3329e676942c8d52a3731fba0c44ab2.js
@@ -0,0 +1,94 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8e3ec8ab495f9450f20ea4a691380c96",
+    "url": "/vaartani--forms/index.html"
+  },
+  {
+    "revision": "f70e77fcc3d29ba7890d",
+    "url": "/vaartani--forms/static/css/2.53a3b0d0.chunk.css"
+  },
+  {
+    "revision": "7a6f664d6bd6f1e02ec0",
+    "url": "/vaartani--forms/static/css/main.2cb98a38.chunk.css"
+  },
+  {
+    "revision": "f70e77fcc3d29ba7890d",
+    "url": "/vaartani--forms/static/js/2.316f9729.chunk.js"
+  },
+  {
+    "revision": "ff404e9534e4e1c0285e6268d367e0c5",
+    "url": "/vaartani--forms/static/js/2.316f9729.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7a6f664d6bd6f1e02ec0",
+    "url": "/vaartani--forms/static/js/main.41933adf.chunk.js"
+  },
+  {
+    "revision": "a060a5e0beafbacfba0f",
+    "url": "/vaartani--forms/static/js/runtime-main.51ab758d.js"
+  },
+  {
+    "revision": "13db00b7a34fee4d819ab7f9838cc428",
+    "url": "/vaartani--forms/static/media/brand-icons.13db00b7.eot"
+  },
+  {
+    "revision": "a046592bac8f2fd96e994733faf3858c",
+    "url": "/vaartani--forms/static/media/brand-icons.a046592b.woff"
+  },
+  {
+    "revision": "a1a749e89f578a49306ec2b055c073da",
+    "url": "/vaartani--forms/static/media/brand-icons.a1a749e8.svg"
+  },
+  {
+    "revision": "c5ebe0b32dc1b5cc449a76c4204d13bb",
+    "url": "/vaartani--forms/static/media/brand-icons.c5ebe0b3.ttf"
+  },
+  {
+    "revision": "e8c322de9658cbeb8a774b6624167c2c",
+    "url": "/vaartani--forms/static/media/brand-icons.e8c322de.woff2"
+  },
+  {
+    "revision": "9c74e172f87984c48ddf5c8108cabe67",
+    "url": "/vaartani--forms/static/media/flags.9c74e172.png"
+  },
+  {
+    "revision": "0ab54153eeeca0ce03978cc463b257f7",
+    "url": "/vaartani--forms/static/media/icons.0ab54153.woff2"
+  },
+  {
+    "revision": "8e3c7f5520f5ae906c6cf6d7f3ddcd19",
+    "url": "/vaartani--forms/static/media/icons.8e3c7f55.eot"
+  },
+  {
+    "revision": "962a1bf31c081691065fe333d9fa8105",
+    "url": "/vaartani--forms/static/media/icons.962a1bf3.svg"
+  },
+  {
+    "revision": "b87b9ba532ace76ae9f6edfe9f72ded2",
+    "url": "/vaartani--forms/static/media/icons.b87b9ba5.ttf"
+  },
+  {
+    "revision": "faff92145777a3cbaf8e7367b4807987",
+    "url": "/vaartani--forms/static/media/icons.faff9214.woff"
+  },
+  {
+    "revision": "701ae6abd4719e9c2ada3535a497b341",
+    "url": "/vaartani--forms/static/media/outline-icons.701ae6ab.eot"
+  },
+  {
+    "revision": "82f60bd0b94a1ed68b1e6e309ce2e8c3",
+    "url": "/vaartani--forms/static/media/outline-icons.82f60bd0.svg"
+  },
+  {
+    "revision": "ad97afd3337e8cda302d10ff5a4026b8",
+    "url": "/vaartani--forms/static/media/outline-icons.ad97afd3.ttf"
+  },
+  {
+    "revision": "cd6c777f1945164224dee082abaea03a",
+    "url": "/vaartani--forms/static/media/outline-icons.cd6c777f.woff2"
+  },
+  {
+    "revision": "ef60a4f6c25ef7f39f2d25a748dbecfe",
+    "url": "/vaartani--forms/static/media/outline-icons.ef60a4f6.woff"
+  }
+]);
